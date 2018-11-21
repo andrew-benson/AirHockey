8d7a3e44f945fe6bb0220674b1e7ee956840b18a
andrewbenson <andrew.benson@grahambrown.com>
2018-11-21 02:58:24 +0000

    Added colliders for goals and win conditions

---
+++ ActivTable/Assets/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public int goalsNeededToWin = 7;
+
+    public enum Score
+    {
+        Player1Scored, Player2Scored
+    }
+
+    public Text p1Txt, p2Txt, gameOverText;
+    private int currentPlayer1Score, currentPlayer2Score;
+
+    public int CurrentPlayer1Score
+    {
+        get { return currentPlayer1Score; }
+        private set { currentPlayer1Score = value; }
+    }
+
+    public int CurrentPlayer2Score
+    {
+        get
+        {
+            return currentPlayer2Score;
+        }
+
+        private set
+        {
+            currentPlayer2Score = value;
+        }
+    }
+
+    public void Increment(Score playerScored)
+    {
+        if (playerScored == Score.Player1Scored)
+            p1Txt.text = (++CurrentPlayer1Score).ToString();
+        else
+            p2Txt.text = (++CurrentPlayer2Score).ToString();
+
+        if(CurrentPlayer1Score == goalsNeededToWin)
+        {
+            gameOverText.enabled = true;
+            gameOverText.text += "Player 1";
+        }
+
+        if (CurrentPlayer2Score == goalsNeededToWin)
+        {
+            gameOverText.enabled = true;
+            gameOverText.text += "Player 2";
+        }
+    }
+}
---
+++ ActivTable/Assets/Puck.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puck : MonoBehaviour {
+
+    public GameManager manager;
+    public static bool WasGoal { get; private set; }
+    private Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!WasGoal)
+        {
+            if (other.tag == "P1 Goal")
+            {
+                manager.Increment(GameManager.Score.Player2Scored);
+                WasGoal = true;
+
+                if(manager.CurrentPlayer2Score == manager.goalsNeededToWin)
+                {
+                    StartCoroutine(ResetGame());
+                }
+                else
+                {
+                    StartCoroutine(ResetPuck());
+                }
+            }
+            else if (other.tag == "P2 Goal")
+            {
+                manager.Increment(GameManager.Score.Player1Scored);
+                WasGoal = true;
+
+                if (manager.CurrentPlayer1Score == manager.goalsNeededToWin)
+                {
+                    StartCoroutine(ResetGame());
+                }
+                else
+                {
+                    StartCoroutine(ResetPuck());
+                }
+
+            }
+        }
+    }
+
+    private IEnumerator ResetPuck()
+    {
+        yield return new WaitForSecondsRealtime(1);
+        WasGoal = false;
+        rb.velocity = rb.position = new Vector2(0, 0);
+    }
+
+    private IEnumerator ResetGame()
+    {
+        GetComponent<Renderer>().enabled = false;
+        yield return new WaitForSecondsRealtime(5);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+}
