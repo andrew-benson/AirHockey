68af6e52af2eb6f9b992049ef123a69db7f16af9
andrewbenson <andrew.benson@grahambrown.com>
2020-05-19 17:32:23 +0100

    Added an improved touch script, uses linq

---
+++ ActivTable/Assets/Scripts/TouchManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class TouchManager : MonoBehaviour {
+
+    private Vector3 playerSize;
+    Rigidbody2D rigidbody2d;
+
+    // Use this for initialization
+    void Start () {
+        playerSize = gameObject.GetComponent<SpriteRenderer>().bounds.extents;
+        rigidbody2d = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if(Input.touchCount > 0)
+        {
+            var touchPositions = Input.touches.Select(t => Camera.main.ScreenToWorldPoint(t.position)).ToList();
+
+            var touchedInside = touchPositions.Where(t => (t.x >= transform.position.x && t.x < transform.position.x + playerSize.x ||
+            t.x <= transform.position.x && t.x > transform.position.x - playerSize.x) &&
+            (t.y >= transform.position.y && t.y < transform.position.y + playerSize.y ||
+            t.y <= transform.position.y && t.y > transform.position.y - playerSize.y));
+
+            if (touchedInside.Any())
+            {
+                var touchPos = touchedInside.ToList().First();            
+
+                rigidbody2d.MovePosition(touchPos);
+            }
+        }
+	}
+}
